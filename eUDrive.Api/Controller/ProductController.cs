f424908d4afca14f4d49e353c883f88c02e50026
luvGrinn <alexei.donoga@isa.utm.md>
2026-04-03 11:49:19 +0300

    fix

---
+++ eUDrive.Api/Controller/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eUDrive.Api.Controller
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("getAll")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
