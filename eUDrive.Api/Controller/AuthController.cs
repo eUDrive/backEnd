736969087e2cdc223db9a6d5d914a783c1b3936c
luvGrinn <alexei.donoga@isa.utm.md>
2026-05-06 16:40:32 +0300

    AuthController edit

--- eUDrive.Api/Controller/AuthController.cs
+++ eUDrive.Api/Controller/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using eUDrive.BusinessLogic.Interfaces;
 using eUDrive.Domains.Models.User;
 using eUDrive.DataAccess.Context;
@@ -89,5 +90,46 @@ namespace eUDrive.Api.Controller
             return Ok(registerUser);
         }
 
+        [HttpGet("check")]
+        public IActionResult CheckSession()
+        {
+            var cookie = Request.Cookies["X-KEY"];
+
+            if (string.IsNullOrWhiteSpace(cookie))
+            {
+                return Unauthorized(new { IsSuccess = false, message = "Not authenticated" });
+            }
+
+            using (var db = new UserContext())
+            {
+                var session = db.Sessions.FirstOrDefault(s => s.SessionKey == cookie && s.IsActive);
+
+                if (session == null || session.ExpiryTime < DateTime.Now)
+                {
+                    return Unauthorized(new { IsSuccess = false, message = "Session expired" });
+                }
+
+                var user = db.Users.FirstOrDefault(u => u.Id == session.UserId);
+
+                if (user == null)
+                {
+                    return Unauthorized(new { IsSuccess = false, message = "User not found" });
+                }
+
+                return Ok(new
+                {
+                    IsSuccess = true,
+                    data = new
+                    {
+                        user = new
+                        {
+                            id = user.Id,
+                            username = user.Username,
+                            email = user.Email
+                        }
+                    }
+                });
+            }
+        }
     }
 }
